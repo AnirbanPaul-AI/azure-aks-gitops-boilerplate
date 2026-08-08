2be2f524eea4cca8a886f221c50c527c54171b6c
root <root@anirban-test.zhycokp32ihuplupm4moojcvlh.tx.internal.cloudapp.net>
2026-08-08 20:19:51 +0000

    Status 3 - AKS+ACR infra with clean gitignore

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,35 +1,57 @@
-provider "azurerm" { features {} }
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 3.90"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
 
 resource "azurerm_resource_group" "rg" {
-  name = var.resource_group_name
+  name     = var.resource_group_name
   location = var.location
 }
 
-resource "random_id" "id" { byte_length = 2 }
+resource "random_id" "id" {
+  byte_length = 2
+}
 
 resource "azurerm_container_registry" "acr" {
-  name = "acrgitosprod${random_id.id.hex}"
+  name                = "acrgitosprod${random_id.id.hex}"
   resource_group_name = azurerm_resource_group.rg.name
-  location = azurerm_resource_group.rg.location
-  sku = "Basic"
-  admin_enabled = true
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name = var.aks_cluster_name
-  location = azurerm_resource_group.rg.location
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix = "aks-gitops"
+  dns_prefix          = "aks-gitops"
+
   default_node_pool {
-    name = "default"
+    name       = "default"
     node_count = var.node_count
-    vm_size = var.vm_size
+    vm_size    = var.vm_size
   }
-  identity { type = "SystemAssigned" }
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
 
 resource "azurerm_role_assignment" "aks_acr" {
-  principal_id = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   role_definition_name = "AcrPull"
-  scope = azurerm_container_registry.acr.id
+  scope                = azurerm_container_registry.acr.id
 }
